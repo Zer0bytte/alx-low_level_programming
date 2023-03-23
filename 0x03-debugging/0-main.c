915743ce669a353e980ab9694bdd4b1ad728b544
Zer0byte <mekomedhat682@gmail.com>
2023-03-23 02:00:26 -0700

    Errors

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,24 @@
-#include "main.h"
+#include <stdio.h>
+
 /**
-* main - Test function for positive or negative
+* main - causes an infinite loop
 * Return: 0
 */
+
 int main(void)
 {
-int i;
-i = 0;
-positive_or_negative(i);
-return (0);
-}
+        int i;
 
+        printf("Infinite loop incoming :(\n");
+
+        i = 0;
+/*
+        while (i < 10)
+        {
+                putchar(i);
+        }
+*/
+        printf("Infinite loop avoided! \\o/\n");
+
+        return (0);
+}
