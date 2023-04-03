218195efb42513286813cb565d5af4ecfac9102b
Zer0byte <mekomedhat682@gmail.com>
2023-04-03 06:27:10 -0700

    0-memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,11 @@
 #include "main.h"
+/**
+ * _memset - fill a block of memory with a specific value
+ * @s: starting address of memory to be filled
+ * @b: the desired value
+ * @n: number of bytes to be changed
+ * Return: chanaged aray with new value for n bytes
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
 	int i;
@@ -9,4 +16,5 @@ char *_memset(char *s, char b, unsigned int n)
 		n--;
 	}
 	return (s);
+}
 }
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+/**
+ * File: main
+ *
+ * Desc: header file
+ */
 char *_memset(char *s, char b, unsigned int n);
 int _putchar(char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
